a9769d8dfab55e853f1060e6926f7806be329595
deLordeee <evgeniy.one.2013@gmail.com>
2025-02-28 17:29:49 +0200

    Added imports and timesResponse

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
-package main 
+package main
 
+import (
+    "encoding/json"
+    "net/http"
+    "time"
+)
+
+type TimeResponse struct {
+    Time string `json:"time"`
+}
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
     response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
@@ -11,5 +20,3 @@ func main() {
     http.HandleFunc("/time", timeHandler)
     http.ListenAndServe(":8795", nil)
 }
-
-
