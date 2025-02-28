aa50a27944ae48894c0602d55a7b8710633ea7e7
ferer00 <ferer004829@gmail.com>
2025-02-28 17:39:33 +0200

    added imeHandler

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
-'package main 
+package main 
+
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+    response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+    http.HandleFunc("/time", timeHandler)
+    http.ListenAndServe(":8795", nil)
+}
+
+
